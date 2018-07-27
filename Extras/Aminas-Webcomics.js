5e255f882a98b3e15b6e6cfdad50992df08c3f28
Ducky <39009745+Felhzverse@users.noreply.github.com>
2018-07-27 00:54:26 -0400

    Add files via upload

---
+++ Extras/Aminas-Webcomics.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+
+// fantasy, comedy, slice of life, adventure, romance, lgbt, 
+function y() {
+    var x = document.getElementByClass("y");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
