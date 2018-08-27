f81ff91bc07f46d126094d5df1b5122efa851371
Ducky <39009745+Felhzverse@users.noreply.github.com>
2018-08-27 03:49:44 -0400

    Add files via upload

--- buttons/DTIS-Pages.js
+++ buttons/DTIS-Pages.js
@@ -63,6 +63,18 @@ function latest(){
 	   	   document.location=pageNumber[i]; 
 }
 
+//PAGE SHIT
+function altText() {
+	"use strict";
+    var desc = document.getElementById("page").alt;
+    window.alert(desc); 
+}
+
+document.getElementById("page").onclick = function() {
+"use strict"; 
+	altText();};
+
+//EXECUTE
 document.getElementById("first").onclick = function() {
 "use strict"; 
 	first();};
