37483d1b4c11c5213468bf206600fff7c79832de
Ducky <39009745+Felhzverse@users.noreply.github.com>
2018-08-27 00:36:05 -0400

    Add files via upload

--- buttons/DTIS-Pages.js
+++ buttons/DTIS-Pages.js
@@ -5,27 +5,34 @@
 //current pages: 3
 
 // Pages array (include links)
-var pageNumber = ["https://felhzverse.github.io/website/dtis/DTIS-home.html", "https://felhzverse.github.io/website/dtis/A1/Site/DTIS-02.html", "https://felhzverse.github.io/website/dtis/A1/Site/DTIS-03.html",];
-var thisPage= location.href;  
-var latest= pageNumber.length;
+var pageNumber = ["../../A1/Site/DTIS-01.html", "../../A1/Site/DTIS-02.html", "../../A1/Site/DTIS-03.html",];
+var i= (pageNumber.length-1);
+var thisPage; 
+//ENABLE BUTTONS
+document.getElementById("first").disabled = false;
+document.getElementById("previous").disabled = false;
+document.getElementById("next").disabled = false;
+document.getElementById("latest").disabled = false;
 //first button
 function first(){
 "use strict"; 
-	   	   window.location=pageNumber[0];  
+	   	   document.location=pageNumber[0]; 
 }
 
 //previous button
 function previous(){
 "use strict"; 
 //for loop
-  for(var x=0;x< pageNumber.length; x++){	
-//go to
- if(thisPage===(pageNumber[x])){ 
-		window.location=pageNumber[(x-1)];
-	 window.alert(pageNumber[x-1]); 
- }
-}
-}
+thisPage=thisPage-1;
+window.alert(thisPage);
+if (thisPage <=0) { 
+window.alert("Welcome back to the start!"); 
+} 
+else{ 
+window.alert(thisPage);
+window.alert(pageNumber[thisPage]);
+document.location=pageNumber[thisPage]; 
+} }
 
 //next button
 function next(){
@@ -48,13 +55,20 @@ function next(){
 //latest button
 function latest(){
 "use strict"; 
- 	 if(thisPage === pageNumber[0]){
-		window.location="./A1/Site/DTIS-03.html"; 
-		}
-	else{
-window.location=pageNumber[latest]; 
-}
+	   	   document.location=pageNumber[i]; 
 }
 
+document.getElementById("first").onclick = function() {
+"use strict"; 
+	first();};
+document.getElementById("previous").onclick = function() {
+"use strict"; 
+	previous();};
+document.getElementById("next").onclick = function() {
+"use strict"; 
+	next();};
+document.getElementById("latest").onclick = function() {
+"use strict"; 
+	latest();};
 //end 
 
